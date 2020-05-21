365375e0da6b570b32e428e4609c559cb9b7255e
Hakan Bilgin <hbi99@hotmail.com>
2020-05-21 22:15:35 +0200

    forgot pgn

--- src/js/main.js
+++ src/js/main.js
@@ -12,27 +12,7 @@ import yukon from "./modules/yukon"
 let ENGINES = { freecell, solitaire, spider, yukon },
 	ACTIVE = "solitaire",
 	// for dev purposes
-	pgn = `Spider
-casino,red
-301:cQ-H11,cQ-H63,c8-H20,c9-H21,c3-H15,cK-H38,cK-H25,c2-H27,c2-H40,c3-H2,c10-H22,c6-H96,cJ-H36,c2-H14,c9-H47,cK-H77,c9-H34,cK-H51,c8-H46,c5-H43,cJ-H23,c6-H18,c4-H3,c8-H33,c8-H85,cJ-H88,c5-H82,c6-H5,c5-H56,c10-H74,c7-H84,cA-H39,cK-H64,c6-H31,c4-H81,cQ-H102,cJ-H101,c9-H86,c10-H61,c7-H58,c2-H53,c9-H99,c7-H19,c6-H83,c9-H60,cJ-H75,c3-H80,c4-H29,c7-H97,cJ-H10
-311:cK-S12,cQ-S37,cJ-S49,c10-S48,c9-S73,c8-S72,c7-S71,c6-S44,c5-S17,c4-S16,c3-S67,c2-S66,cA-S26
-312:cK-S103,cQ-S76,cJ-S62,c10-S35,c9-S8,c8-S59,c7-S6,c6-S70,c5-S30,c4-S55,c3-S93,c2-S92,cA-S52
-313:
-314:
-315:
-316:
-317:
-318:
-321:c10-H9,cA-H0,cA-S78
-322:c6-H57,c4-H94,cA-H13,c3-S28
-323:c5-S69
-324:c10-H100,cQ-S89
-325:c5-S95,c4-S42,c3-S54
-326:c5-H4,cQ-H24,c7-H32,c10-S87
-327:c4-H68,c8-H98,c3-S41,c2-S1,cA-S91
-328:
-329:c8-S7,c7-S45
-330:c2-H79,cK-H90,cA-H65,cQ-S50`;
+	pgn = ``;
 
 const app = {
 	init() {
